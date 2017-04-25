e4e625375583c1e55e7e900f02c9f92bd1520d09
Pristine Tran <pristinetran@tycoint.com>
2017-04-25 13:10:49 -0700

    initial commit

---
+++ derek_tut/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace derek_tut
+{
+	class MainClass
+	{
+		static void Main(string[] args)
+		{
+			/* my first program in C# */
+			Console.WriteLine("Voting app");
+			bool amIableToVote = false;
+			amIableToVote = CanIVote();
+			Console.WriteLine("Can I vote ? {0}", amIableToVote);
+		}
+
+		static bool CanIVote()
+		{
+			Console.WriteLine("How old are you?");
+			int age = 0;
+			age = Int32.Parse(Console.ReadLine());
+			return (age >= 18);
+		}
+	}
+}
