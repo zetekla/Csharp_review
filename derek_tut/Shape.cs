9aa6b178b5194772e7ad09b715ecbac759d615bb
Pristine Tran <pristinetran@tycoint.com>
2017-05-14 22:32:28 -0700

    shape area

---
+++ derek_tut/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace derek_tut
+{
+	abstract class Shape
+	{
+		public string Name { get; set; }
+		public virtual void GetInfo() {
+			Console.WriteLine($"This is a {Name}");
+		}
+
+		public abstract double Area();
+	}
+}
