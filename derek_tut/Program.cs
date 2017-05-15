cfd87960ed0b604b8eb6adb5b06d7630cf5287b5
Pristine Tran <pristinetran@tycoint.com>
2017-05-14 21:33:07 -0700

    cleanup

--- derek_tut/Program.cs
+++ derek_tut/Program.cs
@@ -8,10 +8,7 @@ namespace derek_tut
 	{
 		static void Main(string[] args)
 		{
-			Warrior maximus = new Warrior("Maximus", 1000, 120, 40);
-			Warrior bob = new Warrior("Bob", 1000, 120, 40);
-
-			Battle.StartFight(maximus, bob);
+			Console.WriteLine("Hello");
 		}
 
 	}
